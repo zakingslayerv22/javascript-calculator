cae3dd7b93e7306b8a020ba27e52db0b59bfffc4
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-12-11 13:52:46 +0100

    Create variables for the 2 numbers and the operator
    Write the operate() function that will take
    the 2 numbers and performs an operation on
    them by calling any of the basic math operator
    functions (that was written above) on the 2
    numbers. Made it a switch statement so that
    these functions can be called based on the
    operator.

--- javascript.js
+++ javascript.js
@@ -6,4 +6,20 @@ let multiplication = (a, b) => a * b;
 
 let division = (a, b) => a / b;
 
-console.log(division(4, 2));
+let firstNumber, 
+        secondNumber, 
+             operator;
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case "+":
+            return addition(firstNumber, secondNumber);
+        case "-":
+            return subtraction(firstNumber, secondNumber);
+        case "*":
+            return multiplication(firstNumber, secondNumber);
+        case "/":
+            return division(firstNumber, secondNumber);
+    }
+}
+
