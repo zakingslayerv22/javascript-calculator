92ca82878c61efd74f8da0c5575d5838d7992b59
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-12-20 13:09:32 +0100

    Refactor the displayNumbers()
    Rename it to handleNumbers(). Add the
    click event listener to the number buttons and
    if textContent exists, the textContent of the clicked
    button is finally saved to secondNumber and displayed
    on the screen. If an operator is clicked and certain
    conditions are met, the second number is pushed into
    the finalOperands array. The operator is also displayed
    on the screen and pushed into the finalOperands array.
    
    Pretty twisted things happening here.

--- javascript.js
+++ javascript.js
@@ -6,9 +6,6 @@ let multiplication = (a, b) => a * b;
 
 let division = (a, b) => a / b;
 
-let firstNumber, 
-        secondNumber, 
-             operator;
 
 function operate(operator, firstNumber, secondNumber) {
     switch (operator) {
@@ -23,17 +20,113 @@ function operate(operator, firstNumber, secondNumber) {
     }
 }
 
+let firstNumber = "",
+        secondNumber = "",
+            operator;
+
+let singleOperand = [];
+let finalOperands = [];
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
 const display = document.querySelector('#screen');
 
-let allNumbers = document.querySelectorAll('.number');
 
-function displayNumbers() {
-    for (let i = 0; i < allNumbers.length; i++) {
-        allNumbers[i].addEventListener('click', () => {
-            console.log("hello");
-            display.textContent += allNumbers[i].textContent;
+function handleCalculations() {
+
+
+    for (let i = 0; i < numberButtons.length; i++) {
+        numberButtons[i].addEventListener("click", () => {
+            let buttonText = numberButtons[i].textContent;
+
+            if (buttonText) {
+                firstNumber += buttonText;
+                secondNumber = +firstNumber
+                console.log(secondNumber)
+                display.textContent = secondNumber;
+            }
+
         });
     }
+
+
+    for (let j = 0; j < operatorButtons.length; j++) {
+        operatorButtons[j].addEventListener("click", () => {
+            let operatorButtonText = operatorButtons[j].textContent;
+            // display.textContent += operatorButtonText
+
+            if (operatorButtonText) {
+                operator = operatorButtonText;
+                if (finalOperands.length == 0 && secondNumber) {
+                    finalOperands[0] = secondNumber;
+                    finalOperands.push(operator);
+                    console.log (finalOperands.length);
+                    console.log(finalOperands)
+                    
+                    firstNumber = "";
+                }else if (finalOperands.length > 0 && secondNumber) {
+                    finalOperands.push(secondNumber);
+                    console.log(finalOperands)
+                    // if i comment the next 2 lines out, it still works
+                    secondNumber = "";
+                    firstNumber = "";
+                    
+                }
+            }
+        });
+    }
+
+
 }
 
-displayNumbers();
+handleCalculations()
+
+// console.
+// console.log(numberButtons);
+
+console.log(finalOperands);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (operator) {
+//     secondNumber += numberButtons[i].textContent;
+//     document.querySelector(".second-number").textContent = secondNumber;
+// } else {
+//     firstNumber += numberButtons[i].textContent;
+//     document.querySelector(".first-number").textContent = firstNumber;
+// }
+// console.log (firstNumber, secondNumber, operator);
+
+// if (allNumbers[i].textContent === 'C') {
+//     display.textContent = '';
+// } else if (allNumbers[i].textContent === 'CE') {
+//     display.textContent = display.textContent.slice(0, -1);
+// } else {
+//     display.textContent += allNumbers[i].textContent;
+// }
+
+
+// console.log(singleOperand);
